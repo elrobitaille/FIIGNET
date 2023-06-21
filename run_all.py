5d656d626b1f76262bc54ba2fde91aaa3e227100
elrobtaille@gmail.com <elrobitaille@gmail.com>
2023-06-21 15:26:42 +0800

    Adding more pipeline processing for GAN/SD

--- run_all.py
+++ run_all.py
@@ -2,9 +2,10 @@
 # Be sure to fix the directories to where you would like them to go.
 
 import subprocess
+import argparse
 
-def main():
-    raw_data_path = "/home/ugrad/serius/edgarrobitaille/AquaFinity-main/fish-data/data/healthy/images"
+def main(use_gpu, model, disease):
+    raw_data_path = "/home/ugrad/serius/edgarrobitaille/test"
     raw_data_enhanced_path = "/home/ugrad/serius/edgarrobitaille/FIIGNET/image_data/processed/raw_image_enhanced"
     resized_data_path = "/home/ugrad/serius/edgarrobitaille/FIIGNET/image_data/unprocessed/resized"
     yolo_weights = "/home/ugrad/serius/edgarrobitaille/FIIGNET/preprocessing/yolo_tracking/YOLO/runs/detect/yolov8n_results2/weights/best.pt"
@@ -36,5 +37,67 @@ def main():
             print(f"Command '{command}' failed with return code: {process.returncode}")
             break
 
+    if use_gpu:
+        pass
+    else:
+        print("A high-performance GPU is needed to train the GAN, such as a NVIDIA Tesla P100 GPU or better. Please refer to the instructions in the README.md file for more information. ")
+
+    if model == 'gan' and use_gpu:
+        print("Running GAN code")
+        if disease == 'ich':
+            pass
+        elif disease == 'red_spot':
+            pass
+       
+    elif model == 'stable_diffusion' and use_gpu:
+        print("Running Stable Diffusion code")
+        if disease == 'ich':
+            pass
+        elif disease == 'red_spot':
+            pass
+        
+    elif model == 'cvae' and use_gpu:
+        print("Running CVAE code")
+        if disease == 'ich':
+            pass
+        elif disease == 'red_spot':
+            pass
+       
+    elif model == 'fiignet' and use_gpu:
+        print("Running FIIGNET code")
+        if disease == 'ich':
+            pass
+        elif disease == 'red_spot':
+            pass
+
+    print("Full pipeline complete!")
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--gpu', dest='gpu', action='store_true',
+                        help='use GPU for processing')
+
+    # Model group
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--gan', dest='model', action='store_const', const='gan',
+                        help='run GAN model')
+    group.add_argument('--stable_diffusion', dest='model', action='store_const', const='stable_diffusion',
+                        help='run Stable Diffusion model')
+    group.add_argument('--cvae', dest='model', action='store_const', const='cvae',
+                        help='run CVAE model')
+    group.add_argument('--fiignet', dest='model', action='store_const', const='fiignet',
+                        help='run FIIGNET model')
+
+    # Diseases group
+    group_diseases = parser.add_mutually_exclusive_group(required=True)
+    group_diseases.add_argument('--ich', dest='disease', action='store_const', const='ich',
+                        help='run code for ICH')
+    group_diseases.add_argument('--red_spot', dest='disease', action='store_const', const='red_spot',
+                        help='run code for Red Spot')
+
+    args = parser.parse_args()
+    main(args.gpu, args.model, args.disease)
+
+
+
+
