fa53a006c6e7f6c68a600d1df17d5ca3ebd38f3a
elrobtaille@gmail.com <elrobitaille@gmail.com>
2023-07-01 06:17:32 +0800

    Making updates to train

--- classification/train.py
+++ classification/train.py
@@ -8,24 +8,28 @@ import argparse
 def main(args):
     path = args.input_path
 
-    data = ImageDataLoaders.from_folder(path, train=".", valid_pct=0.2, 
-                                        item_tfms=Resize(460),
-                                        batch_tfms=[*aug_transforms(size=224, min_scale=0.75), 
-                                                    Normalize.from_stats(*imagenet_stats),
-                                                    FlipItem(p=0.5)])
+    data = ImageDataLoaders.from_folder(path, train='train', valid_pct=0.2,
+                                    item_tfms=Resize(460),
+                                    batch_tfms=[*aug_transforms(size=224, min_scale=0.75), 
+                                                Normalize.from_stats(*imagenet_stats),
+                                                FlipItem(p=0.5)])
 
-    learn = cnn_learner(data, resnet152, metrics=accuracy)
+
+    learn = cnn_learner(data, resnet152, metrics=accuracy, wd=0.1)
+
+    learn.model = nn.Sequential(*[learn.model, nn.Dropout(0.5)])
 
     learn.lr_find()
 
     learn.recorder.plot_lr_find()
 
-    lr = 1e-4
+    lr = 1e-5
 
     learn.fit_one_cycle(4, slice(lr))
 
     learn.unfreeze()
-    learn.fit_one_cycle(30, slice(lr/100, lr))
+    
+    learn.fit_one_cycle(100, slice(lr/100, lr))
 
     interp = ClassificationInterpretation.from_learner(learn)
     interp.plot_confusion_matrix()
