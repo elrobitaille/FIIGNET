baff8b2509cb200bbc304b47a34d234792ffad68
elrobtaille@gmail.com <elrobitaille@gmail.com>
2023-07-22 01:41:52 +0800

    Cleaning up run all and added config file

--- run_all.py
+++ run_all.py
@@ -1,47 +1,55 @@
 import subprocess
 import argparse
+import json
 
-def main(model, background):
-    raw_data_path = "/home/ugrad/serius/edgarrobitaille/invokeai/outputs/ich_fakes" 
-    raw_data_enhanced_path = "image_data/processed/raw_image_enhanced" 
-    resized_data_path = "image_data/unprocessed/resized" 
-    facebook_checkpts = "/home/ugrad/serius/edgarrobitaille/segment-anything/sam_vit_l_0b3195.pth"
-    facebook_mask_output = "preprocessing/mask_image"
-    facebook_color_output = "preprocessing/mask_image/colored_masks"
-    cropped_data_path = "image_data/unprocessed/segmented"
-    square_resized_path = "image_data/processed/square_resize"
-    colored_mask_path = "preprocessing/mask_image/colored_masks"
+def load_config(config_file="config.json"):
+    with open(config_file, "r") as file:
+        return json.load(file)
 
-    resize_height = 256
-    resize_width = 256
-    square_size = 256
+CONFIG = load_config()
 
-    augmented_data_path = "/home/ugrad/serius/edgarrobitaille/FIIGNET/image_data/unprocessed/augmented"
+IMAGE_DATA_PATH = f"{CONFIG['base_path']}/image_data"
+UNPROCESSED_PATH = f"{IMAGE_DATA_PATH}/unprocessed"
+PROCESSED_PATH = f"{IMAGE_DATA_PATH}/processed"
 
-    gan_output = "/home/ugrad/serius/edgarrobitaille/FIIGNET/GAN_output"
-    stable_diffusion_output = "/home/ugrad/serius/edgarrobitaille/FIIGNET/SD_output"
+resize_height = CONFIG['resize']['height']
+resize_width = CONFIG['resize']['width']
+square_size = CONFIG['square_size']
 
+PATHS = {
+    'raw_data': f"{UNPROCESSED_PATH}/raw/ich",
+    'augmented_data': f"{UNPROCESSED_PATH}/augmented",
+    'cropped_data': f"{UNPROCESSED_PATH}/segmented",
+    'square_resized': f"{PROCESSED_PATH}/square_resize",
+    'mask_output': "preprocessing/mask_image",
+    'mask_color_output': "preprocessing/mask_image/colored_masks",
+    'facebook_checkpts': f"{CONFIG['segment_anything_path']}/sam_vit_l_0b3195.pth",
+    'results': f"{CONFIG['base_path']}/results",
+}
+
+def generate_commands(model, skip_background):
     commands = [
-       # f"make inflate input_path={raw_data_path} output_path={augmented_data_path}",
-       f"make crop input_path={raw_data_path} output_path={cropped_data_path}"
+        # f"make inflate input_path={PATHS['raw_data']} output_path={PATHS['augmented_data']}",
+        f"make crop input_path={PATHS['raw_data']} output_path={PATHS['cropped_data']}"
     ]
 
-    if not background:
-        commands.append(f"make mask input_path={cropped_data_path} checkpoint_path={facebook_checkpts} output_path={facebook_mask_output}")
+    if not skip_background:
+        commands.append(f"make mask input_path={PATHS['cropped_data']} checkpoint_path={PATHS['facebook_checkpts']} output_path={PATHS['mask_output']}")
 
     if model == "gan":
         commands.extend([
-            f"make resize input_path={cropped_data_path} output_path={square_resized_path} width={square_size} height={square_size} padding=1",
-            f"make enhance input_path={square_resized_path} output_path={gan_output}",
+            f"make resize input_path={PATHS['cropped_data']} output_path={PATHS['square_resized']} width={square_size} height={square_size} padding=1",
+            f"make enhance input_path={PATHS['square_resized']} output_path={PATHS['results']}"
         ])
+    elif model == "stable_diffusion":
+        enhance_input = PATHS['mask_color_output'] if not skip_background else PATHS['cropped_data']
+        commands.append(f"make enhance input_path={enhance_input} output_path={PATHS['results']}")
 
-    if model == "stable_diffusion":
-        if background:
-            commands.append(f"make enhance input_path={cropped_data_path} output_path={stable_diffusion_output}")
-        else:
-            commands.append(f"make enhance input_path={facebook_color_output} output_path={stable_diffusion_output}")
+    # Extend for future models 
 
+    return commands
 
+def run_commands(commands):
     for command in commands:
         process = subprocess.Popen(command, shell=True)
         process.communicate()  
@@ -52,21 +60,20 @@ def main(model, background):
 
     print("Pipeline Part 1 is complete!")
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
+def main():
+    parser = argparse.ArgumentParser(description='Process images using specified models.')
 
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--gan', dest='model', action='store_const', const='gan',
-                        help='run GAN model')
-    group.add_argument('--stable_diffusion', dest='model', action='store_const', const='stable_diffusion',
-                        help='run Stable Diffusion model')
-    group.add_argument('--cvae', dest='model', action='store_const', const='cvae',
-                        help='run CVAE model')
-    group.add_argument('--fiignet', dest='model', action='store_const', const='fiignet',
-                        help='run FIIGNET model')
+    models = ['gan', 'stable_diffusion', 'cvae', 'fiignet']
+    for model in models:
+        group.add_argument(f'--{model}', dest='model', action='store_const', const=model, help=f'run {model.upper()} model')
 
-    parser.add_argument('--background', dest='background', action='store_true',
-                        help='skip background removal')
+    parser.add_argument('--background', dest='skip_background', action='store_true', help='skip background removal')
 
     args = parser.parse_args()
-    main(args.model, args.background)
+
+    commands = generate_commands(args.model, args.skip_background)
+    run_commands(commands)
+
+if __name__ == "__main__":
+    main()
